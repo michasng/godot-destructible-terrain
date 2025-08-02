8a190b8de5cc58b658d9d60fe3282889cd8bfdfa
Micha Sengotta <micha.sengotta@gmail.com>
2025-08-02 11:46:36 +0200

    Fix resolving polygon holes

--- scripts/test_polygons.gd
+++ scripts/test_polygons.gd
@@ -1,32 +1,28 @@
 class_name TestPolygons
 extends Node
 
+@onready var static_body: StaticBody2D = $StaticBody2D
+@onready var outer_chunk_polygon: ChunkPolygon = $StaticBody2D/OuterChunkPolygon
+@onready var inner_chunk_polygon: ChunkPolygon = $StaticBody2D/InnerChunkPolygon
+@onready var result_chunk_polygon: ChunkPolygon = $StaticBody2D/ResultChunkPolygon
+
+@onready var ChunkPolygonScene: PackedScene = preload("res://scenes/chunk_polygon.tscn")
+
 func _ready():
 	_test_resolve_holes()
 
 
 func _test_resolve_holes():
-	var outer = [
-		Vector2(500, 0),
-		Vector2(500, 500),
-		Vector2(0, 500),
-		Vector2(0, 0),
-	]
+	var outer_polygon := outer_chunk_polygon.transform * outer_chunk_polygon.polygon
+	var inner_polygon := inner_chunk_polygon.transform * inner_chunk_polygon.polygon
 	
-	var inner1 = [
-		Vector2(100, 200),
-		Vector2(200, 200),
-		Vector2(200, 100),
-		Vector2(100, 100),
-	]
-	
-	var inner2 = [
-		Vector2(300, 400),
-		Vector2(400, 400),
-		Vector2(400, 300),
-		Vector2(300, 300),
-	]
-	
-	var result = Polygons.resolve_holes([outer, inner2, inner1])
-	print(result)
-	$Polygon2D.polygon = result[0]
+	var clipped_polygons := Polygons.clip(outer_polygon, inner_polygon)
+	_show_result(clipped_polygons)
+
+
+func _show_result(result: Array[PackedVector2Array]) -> void:
+	print("result: ", result)
+	for polygon in result:
+		var chunk_polygon: ChunkPolygon = ChunkPolygonScene.instantiate()
+		static_body.add_child(chunk_polygon)
+		chunk_polygon.update_polygon(polygon)
